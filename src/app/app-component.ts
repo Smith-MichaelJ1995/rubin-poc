ea6045a6579917ef88fc563e3497eba19a4f408d
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2020-10-20 20:55:18 -0400

    Incorporated basic POC of fuse fuzzy string searching library.

--- src/app/app-component.ts
+++ src/app/app-component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import Fuse from 'fuse.js'
 
 
 // Importing Custom Components
@@ -85,10 +86,208 @@ export class AppComponent implements OnInit {
     }
   ];
 
+
+  fuseSearch(questionText: string) {
+
+    let list = [
+      {
+        "title": "Old Man's War",
+        "author": {
+          "firstName": "John",
+          "lastName": "Scalzi"
+        }
+      },
+      {
+        "title": "The Lock Artist",
+        "author": {
+          "firstName": "Steve",
+          "lastName": "Hamilton"
+        }
+      },
+      {
+        "title": "HTML5",
+        "author": {
+          "firstName": "Remy",
+          "lastName": "Sharp"
+        }
+      },
+      {
+        "title": "Right Ho Jeeves",
+        "author": {
+          "firstName": "P.D",
+          "lastName": "Woodhouse"
+        }
+      },
+      {
+        "title": "The Code of the Wooster",
+        "author": {
+          "firstName": "P.D",
+          "lastName": "Woodhouse"
+        }
+      },
+      {
+        "title": "Thank You Jeeves",
+        "author": {
+          "firstName": "P.D",
+          "lastName": "Woodhouse"
+        }
+      },
+      {
+        "title": "The DaVinci Code",
+        "author": {
+          "firstName": "Dan",
+          "lastName": "Brown"
+        }
+      },
+      {
+        "title": "Angels & Demons",
+        "author": {
+          "firstName": "Dan",
+          "lastName": "Brown"
+        }
+      },
+      {
+        "title": "The Silmarillion",
+        "author": {
+          "firstName": "J.R.R",
+          "lastName": "Tolkien"
+        }
+      },
+      {
+        "title": "Syrup",
+        "author": {
+          "firstName": "Max",
+          "lastName": "Barry"
+        }
+      },
+      {
+        "title": "The Lost Symbol",
+        "author": {
+          "firstName": "Dan",
+          "lastName": "Brown"
+        }
+      },
+      {
+        "title": "The Book of Lies",
+        "author": {
+          "firstName": "Brad",
+          "lastName": "Meltzer"
+        }
+      },
+      {
+        "title": "Lamb",
+        "author": {
+          "firstName": "Christopher",
+          "lastName": "Moore"
+        }
+      },
+      {
+        "title": "Fool",
+        "author": {
+          "firstName": "Christopher",
+          "lastName": "Moore"
+        }
+      },
+      {
+        "title": "Incompetence",
+        "author": {
+          "firstName": "Rob",
+          "lastName": "Grant"
+        }
+      },
+      {
+        "title": "Fat",
+        "author": {
+          "firstName": "Rob",
+          "lastName": "Grant"
+        }
+      },
+      {
+        "title": "Colony",
+        "author": {
+          "firstName": "Rob",
+          "lastName": "Grant"
+        }
+      },
+      {
+        "title": "Backwards, Red Dwarf",
+        "author": {
+          "firstName": "Rob",
+          "lastName": "Grant"
+        }
+      },
+      {
+        "title": "The Grand Design",
+        "author": {
+          "firstName": "Stephen",
+          "lastName": "Hawking"
+        }
+      },
+      {
+        "title": "The Book of Samson",
+        "author": {
+          "firstName": "David",
+          "lastName": "Maine"
+        }
+      },
+      {
+        "title": "The Preservationist",
+        "author": {
+          "firstName": "David",
+          "lastName": "Maine"
+        }
+      },
+      {
+        "title": "Fallen",
+        "author": {
+          "firstName": "David",
+          "lastName": "Maine"
+        }
+      },
+      {
+        "title": "Monster 1959",
+        "author": {
+          "firstName": "David",
+          "lastName": "Maine"
+        }
+      }
+    ]
+
+    let settings = {
+      // isCaseSensitive: false,
+      // includeScore: false,
+      // shouldSort: true,
+      // includeMatches: false,
+      // findAllMatches: false,
+      // minMatchCharLength: 1,
+      // location: 0,
+      // threshold: 0.6,
+      // distance: 100,
+      // useExtendedSearch: false,
+      // ignoreLocation: false,
+      // ignoreFieldNorm: false,
+      keys: [
+        "title",
+        "author.firstName"
+      ]
+    };
+    
+    const fuse = new Fuse(list, settings);
+    
+    // Search for the pattern
+    return fuse.search(questionText)
+  
+  }
+
   ngOnInit() {}
 
   onFormSubmit() {
-    console.log('Submitted Question:' + this.myFormGroup.get('question')?.value);
+    // console.log('Submitted Question:' + this.myFormGroup.get('question')?.value);
+    let submittedFormValue = this.myFormGroup.get('question')?.value
+
+    console.log(
+      this.fuseSearch(submittedFormValue)
+    )
   }
 }
 
