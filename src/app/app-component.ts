6224b0f507a4832826df9963564148ed08278392
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2020-10-20 21:57:25 -0400

    Able to populate list of results based on fuse search results.

--- src/app/app-component.ts
+++ src/app/app-component.ts
@@ -10,6 +10,7 @@ import {BuildContentComponent} from './components/build-content/build-content.co
 
 // Importing Custom Models
 import {Template} from './models/Template';
+import {TemplateQueryResponse} from './models/TemplateQueryResponse';
 
 /**
  * @title Highlight the first autocomplete option
@@ -20,31 +21,16 @@ import {Template} from './models/Template';
   styleUrls: ['app-component.css'],
 })
 export class AppComponent implements OnInit {
-  myFormGroup = new FormGroup({
-    question: new FormControl("")
-  });
-  questions: Template[] = [
-    {
-      title: 'How could I ask about job opportunities as a student or recent grad?',
-      description: ''
-    },
-    {
-      title: 'How could I write a thank you letter after an interview?',
-      description: ''
-    },
-    {
-      title: 'How could I introduce myself on Linkedin?',
-      description: ''
-    }
-  ];
-
+  myFormGroup: FormGroup;
+  templateQueryResponses: TemplateQueryResponse[];
+  templateDatabase: Template[];
 
   fuseSearch(questionText: string) {
 
     let settings = {
-      // isCaseSensitive: false,
-      // includeScore: false,
-      // shouldSort: true,
+      isCaseSensitive: false,
+      includeScore: true,
+      shouldSort: true,
       // includeMatches: false,
       // findAllMatches: false,
       // minMatchCharLength: 1,
@@ -60,22 +46,49 @@ export class AppComponent implements OnInit {
       ]
     };
     
-    const fuse = new Fuse(this.questions, settings);
+    const fuse = new Fuse(this.templateDatabase, settings);
     
     // Search for the pattern
     return fuse.search(questionText)
   
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.myFormGroup = new FormGroup({
+      question: new FormControl("")
+    });
+    this.templateQueryResponses = [];
+    this.templateDatabase = [
+      {
+        title: "How To Write A Strong Profile Summary",
+        description: `You have 30 seconds to describe yourself. Can you do it?
+          With a LinkedIn summary, that’s all the time you have. Sorry, no one wants to read your entire work history. Not even a little bit.
+          The summary section requires brevity and critical thinking. You must explain what you’re about and the impact you make on others.`
+      },
+      {
+        title: "How To Ask About Job Opportunities As A Student Or Recent Grad",
+        description: `As you come out of school and approach employers, you need an email to prove your maturity and poise. I have seen too many email pitches from recent grads fall flat. Too much focus on “I’m a hard worker” and “I’m detail oriented.” Forget that stuff.
+        Focus on what you have ACCOMPLISHED. Show your value.`
+      }
+    ];
+  }
 
   onFormSubmit() {
-    // console.log('Submitted Question:' + this.myFormGroup.get('question')?.value);
+    // save the user's submitted input
     let submittedFormValue = this.myFormGroup.get('question')?.value
+    
+    // clear the search results
+    this.templateQueryResponses = [];
 
-    console.log(
-      this.fuseSearch(submittedFormValue)
-    )
+    // search for template based on question text & populate results list
+    this.fuseSearch(submittedFormValue).forEach(result => {
+      this.templateQueryResponses.push(
+        {
+          match: result.score,
+          template: result.item
+        }
+      )
+    })
   }
 }
 
---
+++ src/app/models/TemplateQueryResponse.ts
@@ -0,0 +1,7 @@
+import { Template } from './Template';
+
+// creating an object type
+export interface TemplateQueryResponse {
+    match?: any;
+    template: Template
+}
