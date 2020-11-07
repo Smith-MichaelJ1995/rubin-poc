d791a0cd13705c8992238e1008cb3ca0d7cdfcd4
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2020-11-07 17:42:03 -0500

    Adding essential UI look + feel, added templates placeholder for questions and can re-route to new component on submit.

--- src/app/components/home/homepage.component.ts
+++ src/app/components/home/homepage.component.ts
@@ -16,10 +16,22 @@ import {TemplateQueryResponse} from '../../models/TemplateQueryResponse';
 })
 export class HomepageComponent implements OnInit {
 
-  myFormGroup: FormGroup;
+  templateSearchFormGroup: FormGroup;
   templateQueryResponses: TemplateQueryResponse[];
   templateDatabase: Template[];
   selectedTemplate: Template;
+  templateQueryResponsesFormGroups: FormGroup[];
+  timesOfDay: any[] = [
+    "Morning",
+    "Afternoon",
+    "Evening"
+  ]
+  // <mat-select>
+  //   <mat-option *ngFor="let food of foods" [value]="food.value">
+  //     {{food.viewValue}}
+  //   </mat-option>
+  // </mat-select>
+
 
   // perform fuzzy string search based on question
   fuseSearch(questionText: string) {
@@ -50,40 +62,74 @@ export class HomepageComponent implements OnInit {
   
   }
 
+  fetchRespectiveFormGroupObject(result: TemplateQueryResponse) {
+
+    let index = this.templateQueryResponses.indexOf(result)
+
+    // result of this.templateQueryResponses
+
+    return this.templateQueryResponsesFormGroups[index]
+  }
+
   ngOnInit() {
-    this.myFormGroup = new FormGroup({
+    this.templateSearchFormGroup = new FormGroup({
       question: new FormControl("")
     });
+
     this.templateDatabase = [
+      // {
+      //   title: "Writing A Strong Profile Summary Jobs",
+      //   summary: "You have 30 seconds to describe yourself. Can you do it? This is some additional text that will overflow",
+      //   description: `With a LinkedIn summary, that’s all the time you have. Sorry, no one wants to read your entire work history. Not even a little bit.
+      //     The summary section requires brevity and critical thinking. You must explain what you’re about and the impact you make on others.`
+      // },
+      // {
+      //   title: "How could I ask about job opportunities as a recent college graduate?",
+      //   summary: "Focus on what you have accomplished. Show your value. This is some additional text that is purely random in nature.",
+      //   description: `As you come out of school and approach employers, you need an email to prove your maturity and poise. I have seen too many email pitches from recent grads fall flat. Too much focus on “I’m a hard worker” and “I’m detail oriented.” Forget that stuff.`
+      // },
       {
-        title: "Writing A Strong Profile Summary Jobs",
-        summary: "You have 30 seconds to describe yourself. Can you do it? This is some additional text that will overflow",
-        description: `With a LinkedIn summary, that’s all the time you have. Sorry, no one wants to read your entire work history. Not even a little bit.
-          The summary section requires brevity and critical thinking. You must explain what you’re about and the impact you make on others.`
-      },
-      {
-        title: "How could I ask about job opportunities as a recent college graduate?",
-        summary: "Focus on what you have accomplished. Show your value. This is some additional text that is purely random in nature.",
-        description: `As you come out of school and approach employers, you need an email to prove your maturity and poise. I have seen too many email pitches from recent grads fall flat. Too much focus on “I’m a hard worker” and “I’m detail oriented.” Forget that stuff.`
+        title: "How To Thank Somebody After a Job Interview?",
+        summary: "",
+        description: `You may have nailed the job interview, and the employer is ready to offer you the job. Then…you don’t send a thank-you email. Then…the employer begins to wonder, “Hmm, maybe he’s not so sharp after all.”
+        Never let doubt creep into the employer’s brain. Send a proper thank-you note the same day of the interview so you continue to shine.
+        NOTE: If you’re traveling and can’t send a note the same day, it’s OK to write one the next day.        
+        `
       }
     ];
   }
 
   onFormSubmit() {
     // save the user's submitted input
-    let submittedFormValue = this.myFormGroup.get('question')?.value
+    let submittedFormValue = this.templateSearchFormGroup.get('question')?.value
     
     // clear the search results
     this.templateQueryResponses = [];
+    this.templateQueryResponsesFormGroups = [];
 
     // search for template based on question text & populate results list
     this.fuseSearch(submittedFormValue).forEach(result => {
+
+      // popluate the response objects
       this.templateQueryResponses.push(
         {
           match: result.score,
           template: result.item
         }
       )
+
+      // generate form group objects to capture form input
+      this.templateQueryResponsesFormGroups.push(
+        new FormGroup({
+          questionOne: new FormControl(""),
+          questionTwo: new FormControl(""),
+          questionThree: new FormControl(""),
+        })
+      )
+
+
+
+
     })
 
     console.log(this.templateQueryResponses)
