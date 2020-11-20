c0b07d66fcef3bd5451f47d7e85116b806c4419c
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2020-11-19 19:12:01 -0500

    Added #1 'HOW TO SET UP A NETWORKING MEETING'. Able to print an email successfully.

--- src/app/services/template-database.service.ts
+++ src/app/services/template-database.service.ts
@@ -151,6 +151,79 @@ export class TemplateDatabaseService {
           mat_icon:"face"
         }
       ]
+    },
+    // #1 How To Set Up A Networking Meeting
+    {
+      title: "How To Set Up A Networking Meeting",
+      description: `If you want to make introductions, discuss a project, explore job opportunities or meet for another reason altogether, the template below will allow you to start the conversation.`,
+      message: 'Hi p0,\n\n' +
+      'My name is p1, and p2. I hope you\'re doing well. \n' +
+      'p3 \n' +
+      'p4 \n' +
+      'To give you my quick background, p5. \n' +
+      'Again, it would be great to meet you in person. Please let me know your availability over the next couple of weeks. \n' +
+      'Thanks, p6. I look forward to talking with you. \n\n' +
+      'Best,\n' +
+      'p7',
+      prompts: [
+        {
+          // header: "Step #1: Who is the recipient of this message?",
+          pId: "p0",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #1: Who is the recipient of this message?",
+          mat_placeholder: "Enter the individual's first and last name here (ex: Joe Brown)",
+          mat_icon:"person_outline"
+        },
+        {
+          pId: "p1",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #2: What is your full name name?",
+          mat_placeholder: "Please enter your first & last name here (ex: Mike Smith)",
+          mat_icon:"face"
+        },
+        {
+          pId: "p2",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #3: How are you connected to this person? (Give the person context for the email)",
+          mat_placeholder: "Explain how you're connected (ex: “I am the son of Jim Reeves, your former co-worker” or “I'm a recent grad from Big State University with a degree in computer science”)",
+          mat_icon:"watch_later"
+        },
+        {
+          pId: "p3",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #4: Why do you want to meet this person?",
+          mat_placeholder: "Always describe your intent early (ex: “As I’m new to the job market, I hope we can meet for coffee so I can learn more about your career and job prospects in the field of animation.”)",
+          mat_icon:"work_outline"
+        },
+        {
+          pId: "p4",
+          mat_ff_appearance: "standard", 
+          mat_label: "Step #5: List an example about the person's career path you learned from their website",
+          mat_placeholder: "Start with (ex: “I checked out your bio and see you have a lot of design experience, particularly from your time in Hollywood on big budget animated films. That must have been a fantastic opportunity.”)",
+          mat_icon:"person_pin"
+        },
+        {
+          pId: "p5",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #6: One or two highlights from your own background",
+          mat_placeholder: "What would the person find impressive or interesting? (ex: “I recently completed an internship at Acme Corporation, in which I used the software programs XXXX and XXXX. Here’s a link to my latest animated work.”)",
+          mat_icon:"group_work"
+        },
+        {
+          pId: "p6",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #7: What is the recipient's first-name?",
+          mat_placeholder: "Please enter the recipient's first-name here (ex: Joe)",
+          mat_icon:"face"
+        },
+        {
+          pId: "p7",
+          mat_ff_appearance: "standard",
+          mat_label: "Step #8: Closing Salutation",
+          mat_placeholder: "Please enter your first-name (or perferred name) here",
+          mat_icon:"face"
+        }
+      ]
     }
   ];
 
